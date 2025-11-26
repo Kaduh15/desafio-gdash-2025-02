51cbc5a840acd1cb536db7bbd53d72e7fc268996
Kaduh15 <kadu.silva2014@gmail.com>
2025-11-26 11:52:39 -0300

    feat: implementar worker Go para processamento de mensagens
    - Criar aplicação Go que consome mensagens da fila RabbitMQ
    - Implementar conexão com broker usando biblioteca amqp091-go
    - Configurar consumidor para fila 'weather_logs' com durabilidade
    - Implementar envio de dados para webhook via HTTP POST
    - Adicionar tratamento de erros e logs de processamento
    - Configurar acknowledge manual de mensagens após sucesso
    - Utilizar godotenv para carregar variáveis de ambiente (.env)
    - Adicionar dependências go.mod: godotenv v1.5.1 e amqp091-go v1.10.0
    - Configurar worker para rodar continuamente aguardando mensagens

---
+++ worker/go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.25.4
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/rabbitmq/amqp091-go v1.10.0
+)
---
+++ worker/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+}
+
+func main() {
+	URL_WEBHOOK := os.Getenv("URL_WEBHOOK")
+	BROKER_URL := os.Getenv("BROKER_URL")
+	BROKER_QUEUE := os.Getenv("BROKER_QUEUE")
+
+	conn, err := amqp.Dial(BROKER_URL)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		BROKER_QUEUE,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			fmt.Printf("Received a message: %s\n", d.Body)
+
+			response, err := http.Post(URL_WEBHOOK, string("application/json"), bytes.NewBuffer(d.Body))
+
+			if err != nil {
+				fmt.Printf("Failed to send to webhook: %s\n", err)
+			} else {
+				fmt.Printf("Webhook response status: %s\n", response.Status)
+				response.Body.Close()
+				d.Ack(false)
+			}
+		}
+	}()
+
+	fmt.Println("Waiting for messages. To exit press CTRL+C")
+	<-forever
+
+}
